2da50618ff1b4560814f0aec2db8334d3c8eb633
vitalie.jicol <63916548+vitaliejicol@users.noreply.github.com>
2020-08-28 12:09:49 -0500

    Update prepare-worker.groovy

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,23 +1,23 @@
 properties([
     parameters([
-        string(defaultValue: '', description: 'Please enter VM IP', name: 'nodeIP', trim: true)
+        string(defaultValue: '', description: 'Please enter VM IP', name: 'NodeIP', trim: true)
         ])
     ])
 
-if (nodeIP?.trim()) {
+if (NodeIP?.trim()) {
     node {
         withCredentials([sshUserPrivateKey(credentialsId: 'master-jenkins-private', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
             stage('Init') {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install epel-release -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${NodeIP} yum install epel-release -y'
             }
             stage("Install git") {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install git -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${NodeIP} yum install git -y'
             }
             stage("Install Java"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install java-1.8.0-openjdk-devel -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${NodeIP} yum install java-1.8.0-openjdk-devel -y'
             }
             stage("Install Ansible"){
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install ansible -y'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${NodeIP} yum install ansible -y'
             }
         }
     }
